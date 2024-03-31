bb6f9a86f0809ce6b0a46c1935b076b603861980
David-5-5 <13910767907@139.com>
2024-03-31 17:38:38 +0800

    Add my solution with wrong output

--- src/main/java/dev/morling/onebrc/CalculateAverage_luming.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_luming.java
@@ -20,18 +20,20 @@ import static java.util.stream.Collectors.groupingBy;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collector;
+
 
 /**
  * Copy from baseline 22.697s for 0.1Bs
@@ -103,46 +105,169 @@ public class CalculateAverage_luming {
                 // Add parallel method
                 .parallel()
                 .map(l -> new Measurement(l.split(";")))
+                // The are no obliviously improvements between groupingBy and groupingByConcurrent
                 .collect(groupingBy(m -> m.station(), collector)));
 
         System.out.println(measurements);
     }
 
+    static byte line_separator = '\n';
     static int precessors = Runtime.getRuntime().availableProcessors();
     static ExecutorService executorService = Executors.newFixedThreadPool(precessors);
 
     // ----------------------------------------------------------------------------------------------------------------------
-    public static void solution2() throws IOException {
-
-        Map<String, MeasurementAggregator> collector = new TreeMap<>();
-
+    public static void solution2() throws IOException, InterruptedException, ExecutionException {
+        Map<String, MeasurementAggregator> result = new TreeMap<>();
+        Map<Integer, PartialResult> partials = new TreeMap<>();
         try (RandomAccessFile file = new RandomAccessFile(new File(FILE), "r")) {
             FileChannel channel = file.getChannel();
 
-            final AtomicLong start = new AtomicLong(0);
-            int allocated = 0;
+            int chunk = 0;
             long chunkSize = channel.size() / precessors;
+            int leave = (int) channel.size() % precessors;
+            System.out.println("channel.size() = " + channel.size());
+            System.out.println("precessors = " + precessors);
+            while (chunk++ < precessors) {
+                final Future<PartialResult> future = executorService
+                        .submit(new PartialCallable(channel, chunk - 1, chunkSize, leave));
 
-            while (allocated < precessors) {
-                Future future = executorService.submit(() -> {
-                    try {
-                        read(channel.map(FileChannel.MapMode.READ_ONLY, start.longValue(), chunkSize));
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                partials.put(future.get().chunk, future.get());
+
+            }
+            executorService.shutdown();
+
+            String prefix = "";
+            for (int i=0; i<precessors; i++) {
+                if (i==0) {
+                    result = partials.get(i).partial;
+                    prefix = partials.get(i).lastLine;
+                } else {
+                    prefix += partials.get(i).firstLine;
+
+                    for (String key : partials.get(i).partial.keySet()) {
+                        if (result.containsKey(key)) {
+                            MeasurementAggregator agg1 = result.get(key);
+                            MeasurementAggregator agg2 = partials.get(i).partial.get(key);
+                            agg1.min = Math.min(agg1.min, agg2.min);
+                            agg1.max = Math.max(agg1.max, agg2.max);
+                            agg1.sum += agg2.sum;
+                            agg1.count += agg2.count;
+                        } else {
+                            result.put(key, partials.get(i).partial.get(key));
+                        }
                     }
-                    start.addAndGet(chunkSize);
-                });
+
+                    handle(result, prefix + partials.get(i).firstLine);
+
+                    prefix = partials.get(i).lastLine;
+                }
             }
 
+
+            Map<String, ResultRow> measurements = new TreeMap<>();
+            result = partials.get(1).partial;
+            for (String key : result.keySet()) {
+                ResultRow row = new ResultRow(result.get(key).min, result.get(key).sum/ result.get(key).count, result.get(key).max);
+                measurements.put(key, row);
+            }
+            System.out.println(measurements);
         }
     }
 
-    private static void read(MappedByteBuffer buffer) throws IOException {
-        byte[] data = new byte[buffer.remaining()];
-        System.out.println(new String(data, StandardCharsets.UTF_8));
+    public static class PartialCallable implements Callable<PartialResult> {
+        FileChannel channel;
+        int chunk;
+        long chunkSize;
+        int leave;
+
+        PartialCallable(FileChannel channel, int chunk, long chunkSize, int leave) {
+            this.channel = channel;
+            this.chunk = chunk;
+            this.chunkSize = chunkSize;
+            this.leave = leave;
+        }
+
+        @Override
+        public PartialResult call() throws Exception {
+            long size = chunkSize + (chunk + 1 == precessors ? leave : 0);
+            ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, chunk * chunkSize, size);
+
+            // System.out.println(new String(data, StandardCharsets.UTF_8));
+            PartialResult result = new PartialResult(chunk, buffer);
+            result.groupingBy();
+            return result;
+        }
     }
 
-    public static void main(String[] args) throws IOException {
+    private static void handle(Map<String, MeasurementAggregator> aggs, String line) {
+
+        String[] value = line.split(";");
+        if (value.length < 2) {
+            // empty line
+            // System.err.println("error, can't find two elements of '" + new String(line, StandardCharsets.UTF_8) + "'");
+            return;
+        }
+        if (aggs.containsKey(value[0])) {
+            MeasurementAggregator agg = aggs.get(value[0]);
+            agg.count++;
+            agg.min = Math.min(agg.min, Double.valueOf(value[1]));
+            agg.max = Math.max(agg.min, Double.valueOf(value[1]));
+            agg.sum += Double.valueOf(value[1]);
+        }
+        else {
+            MeasurementAggregator agg = new MeasurementAggregator();
+            agg.count = 1;
+            agg.max = agg.min = agg.sum = Double.valueOf(value[1]);
+            aggs.put(value[0], agg);
+        }
+    }
+
+    public static class PartialResult {
+        int chunk;
+        Map<String, MeasurementAggregator> partial = new TreeMap<>();
+        ByteBuffer buffer;
+        String firstLine = "", lastLine = "";
+
+        PartialResult(int chunk, ByteBuffer buffer) {
+            this.chunk = chunk;
+            this.buffer = buffer;
+
+        }
+
+        public void groupingBy() {
+            byte[] data = new byte[buffer.remaining()];
+            buffer.get(data);
+            int begin = 0;
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] == line_separator) {
+                    byte[] line = new byte[i - begin];
+                    System.arraycopy(data, begin, line, 0, line.length);
+                    // System.out.println(new String(line, StandardCharsets.UTF_8));
+                    if (begin == 0 && chunk != 0) {
+                        firstLine = new String(line, StandardCharsets.UTF_8);
+                    }
+                    else
+                        handle(partial, new String(line, StandardCharsets.UTF_8));
+                    // set begin for next line
+                    begin = i + 1;
+                }
+
+            }
+            // Last
+            byte[] line = new byte[data.length - begin];
+            System.arraycopy(data, begin, line, 0, line.length);
+            if (chunk + 1 != precessors) {
+                lastLine = new String(line, StandardCharsets.UTF_8);
+            }
+            else
+                handle(partial, new String(line, StandardCharsets.UTF_8));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        Long begin = System.currentTimeMillis();
         solution2();
+        System.out.println("Execute : " + (System.currentTimeMillis() - begin));
+        
     }
 }

4b7b15b1104d62ea467e62862a98a84b28bf980c
David-5-5 <13910767907@139.com>
2024-04-04 16:31:15 +0800

    Update solutin 4

--- src/main/java/dev/morling/onebrc/CalculateAverage_luming.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_luming.java
@@ -146,10 +146,13 @@ public class CalculateAverage_luming {
     // ------------------------------------------------------------------------------------------------
     // Sharable static class and method
     // ------------------------------------------------------------------------------------------------
-    static byte line_separator = '\n';
-    final static int processor = Runtime.getRuntime().availableProcessors();
-    static ExecutorService executorService = Executors.newFixedThreadPool(processor);
-    static int threads = processor * 9;
+    final static int BUFFER_SIZE = 108;
+    final static int BLOCK_SIZE = 128 * 1024 * 1024;
+    final static byte LINE_SEPARATOR = '\n';
+    final static int PROCESSORS = Runtime.getRuntime().availableProcessors();
+
+    static ExecutorService executorService = Executors.newFixedThreadPool(PROCESSORS);
+    static int threads = PROCESSORS * 9;
 
     public static class SolutionCallable implements Callable<PartialResult> {
         FileChannel channel;
@@ -208,8 +211,6 @@ public class CalculateAverage_luming {
             int chunk = 0;
             long chunkSize = channel.size() / threads;
             int leave = (int) channel.size() % threads;
-            // System.out.println("channel.size() = " + channel.size());
-            // System.out.println("precessors = " + precessors);
 
             if (chunkSize < 1024) {
                 threads = 1;
@@ -233,8 +234,7 @@ public class CalculateAverage_luming {
                 if (i == 0) {
                     result = partials.get(i).partial;
                     prefix = partials.get(i).lastLine;
-                }
-                else {
+                } else {
                     for (String key : partials.get(i).partial.keySet()) {
                         result.merge(key, partials.get(i).partial.get(key),
                                 combiner);
@@ -265,14 +265,13 @@ public class CalculateAverage_luming {
         result.buffer.get(data);
         int begin = 0;
         for (int i = 0; i < data.length; i++) {
-            if (data[i] == line_separator) {
+            if (data[i] == LINE_SEPARATOR) {
                 byte[] line = new byte[i - begin];
                 System.arraycopy(data, begin, line, 0, line.length);
                 // System.out.println(new String(line, StandardCharsets.UTF_8));
                 if (begin == 0 && result.chunk != 0) {
                     result.firstLine = new String(line, StandardCharsets.UTF_8);
-                }
-                else {
+                } else {
                     String[] part = new String(line, StandardCharsets.UTF_8).split(";");
                     if (part.length == 2)
                         result.partial.merge(part[0], new MeasurementAggregator(Double.parseDouble(part[1])), combiner);
@@ -287,8 +286,7 @@ public class CalculateAverage_luming {
         System.arraycopy(data, begin, line, 0, line.length);
         if (result.chunk + 1 != threads) {
             result.lastLine = new String(line, StandardCharsets.UTF_8);
-        }
-        else {
+        } else {
             String[] part = new String(line, StandardCharsets.UTF_8).split(";");
             if (part.length == 2)
                 result.partial.merge(part[0], new MeasurementAggregator(Double.parseDouble(part[1])), combiner);
@@ -310,8 +308,6 @@ public class CalculateAverage_luming {
             int chunk = 0;
             long chunkSize = channel.size() / threads;
             int leave = (int) channel.size() % threads;
-            // System.out.println("channel.size() = " + channel.size());
-            // System.out.println("precessors = " + precessors);
 
             if (chunkSize < 1024) {
                 threads = 1;
@@ -335,8 +331,7 @@ public class CalculateAverage_luming {
                 if (i == 0) {
                     result = partials.get(i).partial;
                     prefix = partials.get(i).lastLine;
-                }
-                else {
+                } else {
                     for (String key : partials.get(i).partial.keySet()) {
                         result.merge(key, partials.get(i).partial.get(key),
                                 combiner);
@@ -367,7 +362,7 @@ public class CalculateAverage_luming {
         int begin = 0, end = data.length - 1;
         if (result.chunk != 0) {
             for (int i = 0; i < data.length; i++) {
-                if (data[i] == line_separator) {
+                if (data[i] == LINE_SEPARATOR) {
                     byte[] line = new byte[i];
                     System.arraycopy(data, begin, line, 0, line.length);
                     result.firstLine = new String(line, StandardCharsets.UTF_8);
@@ -378,7 +373,7 @@ public class CalculateAverage_luming {
         }
         if (result.chunk + 1 != threads) {
             for (int i = data.length - 1; i >= 0; i--) {
-                if (data[i] == line_separator) {
+                if (data[i] == LINE_SEPARATOR) {
                     end = i;
                     byte[] line = new byte[data.length - end - 1];
                     if (end + 1 < data.length) {
@@ -414,6 +409,14 @@ public class CalculateAverage_luming {
     // ---------------------------- AS FOLLOWING IS SOLUTION 4
     // ---------------------------
     // ---------------------------- Modified from my solution 2 to ags313
+    // 1) Change specified threads to specified BLOCK_SIZE for each thread
+    // Result -- 23.516 s
+    //
+    // 2) Add static method valueOf for class Key, then it can change key
+    // from String to Key instance
+    // Result -- Verify Fail
+    //
+    // 3) Change read line to read byte
     // -------------------------------------------------------------------------------------------------
     private static class Key implements Comparable<Key> {
         private final byte[] value = new byte[NAME_LENGTH_LIMIT_BYTES];
@@ -477,70 +480,141 @@ public class CalculateAverage_luming {
         }
     }
 
+    public static class Solution4Result {
+        int chunk;
+        Map<Key, MeasurementAggregator> partial = new HashMap<>(1024 * 16);
+        ByteBuffer buffer;
+        String firstLine = "", lastLine = "";
+
+        Solution4Result(int chunk, ByteBuffer buffer) {
+            this.chunk = chunk;
+            this.buffer = buffer;
+        }
+
+    }
+
+    public static class Solution4Callable implements Callable<Solution4Result> {
+        FileChannel channel;
+        int chunk;
+        long begin;
+        long size;
+        Consumer<Solution4Result> action;
+
+        Solution4Callable(FileChannel channel, int chunk, long begin, long size, Consumer<Solution4Result> action) {
+            this.channel = channel;
+            this.chunk = chunk;
+            this.begin = begin;
+            this.size = size;
+            this.action = action;
+        }
+
+        @Override
+        public Solution4Result call() throws Exception {
+
+            ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, begin, size);
+
+            // System.out.println(new String(data, StandardCharsets.UTF_8));
+            Solution4Result result = new Solution4Result(chunk, buffer);
+            action.accept(result);
+            return result;
+        }
+    }
+
     public static void solution4() throws IOException, InterruptedException, ExecutionException {
-        Map<String, MeasurementAggregator> result = new TreeMap<>();
-        Map<Integer, PartialResult> partials = new TreeMap<>();
+        Map<Key, MeasurementAggregator> result = new TreeMap<>();
+        Map<Integer, Solution4Result> partials = new TreeMap<>();
         try (RandomAccessFile file = new RandomAccessFile(new File(FILE), "r")) {
             FileChannel channel = file.getChannel();
 
             int chunk = 0;
-            long chunkSize = channel.size() / threads;
-            int leave = (int) channel.size() % threads;
-            // System.out.println("channel.size() = " + channel.size());
-            // System.out.println("precessors = " + precessors);
+            long begin = 0;
+            // System.out.println("channel.size = " + channel.size());
+            while (begin < channel.size()) {
+                long size = Math.min(BLOCK_SIZE, channel.size() - begin);
 
-            if (chunkSize < 1024) {
-                threads = 1;
-                chunkSize = channel.size();
-                leave = 0;
-            }
-            while (chunk++ < threads) {
-                final Future<PartialResult> future = executorService
-                        .submit(new SolutionCallable(channel, chunk - 1, chunkSize, leave, r -> {
-                            groupingBy(r);
+                if (size == BLOCK_SIZE) {
+                    ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, size,
+                            Math.min(BUFFER_SIZE, channel.size() - begin - size));
+                    while (buffer.hasRemaining()) {
+                        byte cur = buffer.get();
+                        size++;
+                        if (cur == LINE_SEPARATOR) {
+                            break;
+                        }
+                    }
+                }
+                // System.out.println("execute thread from " + begin + " and size = " + size);
+                final Future<Solution4Result> future = executorService
+                        .submit(new Solution4Callable(channel, chunk++, begin, size, r -> {
+                            handle4(r);
                         }));
-
+                // Next block
+                begin += size;
                 partials.put(future.get().chunk, future.get());
 
             }
             executorService.shutdown();
 
-            String prefix = "";
-            for (int i = 0; i < threads; i++) {
-                // System.out.prinln("fist line")
+            for (int i = 0; i < chunk; i++) {
                 if (i == 0) {
                     result = partials.get(i).partial;
-                    prefix = partials.get(i).lastLine;
-                }
-                else {
-                    for (String key : partials.get(i).partial.keySet()) {
+                } else {
+                    for (Key key : partials.get(i).partial.keySet()) {
                         result.merge(key, partials.get(i).partial.get(key),
                                 combiner);
                     }
-
-                    String[] line = (prefix + partials.get(i).firstLine).split(";");
-                    if (line.length == 2)
-                        result.merge(line[0], new MeasurementAggregator(Double.parseDouble(line[1])), combiner);
-
-                    prefix = partials.get(i).lastLine;
                 }
             }
 
             Map<String, ResultRow> measurements = new TreeMap<>();
-            // result = partials.get(1).partial;
-            for (String key : result.keySet()) {
+            for (Key key : result.keySet()) {
                 ResultRow row = new ResultRow(result.get(key).min,
                         (Math.round(result.get(key).sum * 10.0) / 10.0) / result.get(key).count, result.get(key).max);
-                measurements.put(key, row);
+                measurements.put(key.toString(), row);
             }
             System.out.println(measurements);
         }
 
     }
 
+    public static void handle4(Solution4Result result) {
+        byte[] data = new byte[result.buffer.remaining()];
+        result.buffer.get(data);
+        int begin = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == LINE_SEPARATOR) {
+                byte[] line = new byte[i - begin];
+                System.arraycopy(data, begin, line, 0, line.length);
+                // System.out.println(new String(line, StandardCharsets.UTF_8));
+                if (begin == 0 && result.chunk != 0) {
+                    result.firstLine = new String(line, StandardCharsets.UTF_8);
+                } else {
+                    String[] part = new String(line, StandardCharsets.UTF_8).split(";");
+                    if (part.length == 2)
+                        result.partial.merge(Key.valueOf(part[0]),
+                                new MeasurementAggregator(Double.parseDouble(part[1])), combiner);
+                }
+                // set begin for next line
+                begin = i + 1;
+            }
+
+        }
+        // Last
+        byte[] line = new byte[data.length - begin];
+        System.arraycopy(data, begin, line, 0, line.length);
+        if (result.chunk + 1 != threads) {
+            result.lastLine = new String(line, StandardCharsets.UTF_8);
+        } else {
+            String[] part = new String(line, StandardCharsets.UTF_8).split(";");
+            if (part.length == 2)
+                result.partial.merge(Key.valueOf(part[0]), new MeasurementAggregator(Double.parseDouble(part[1])),
+                        combiner);
+        }
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         // Long begin = System.currentTimeMillis();
-        solution2();
+        solution4();
         // System.out.println("Execute : " + (System.currentTimeMillis() - begin));
 
     }
@@ -552,8 +626,7 @@ public class CalculateAverage_luming {
             Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
             theUnsafe.setAccessible(true);
             return (Unsafe) theUnsafe.get(Unsafe.class);
-        }
-        catch (NoSuchFieldException | IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }

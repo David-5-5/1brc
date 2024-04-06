253d0c949c43c81c141ba946f6292f47607afed2
David-5-5 <13910767907@139.com>
2024-04-06 19:29:38 +0800

    Update the default is no image

--- prepare_tonivade.sh
+++ prepare_tonivade.sh
@@ -21,7 +21,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.2-graal 1>&2
 
 
-if [ ! -f target/CalculateAverage_tonivade_image ] && [ -z "$NOIMAGE"]; then
+if [ ! -f target/CalculateAverage_tonivade_image ] && [ "$WITH_IMAGE" == "1" ]; then
     NATIVE_IMAGE_OPTS="-O3 -march=native --enable-preview"
     native-image $NATIVE_IMAGE_OPTS -cp target/average-1.0.0-SNAPSHOT.jar -o target/CalculateAverage_tonivade_image dev.morling.onebrc.CalculateAverage_tonivade
 fi

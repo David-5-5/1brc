9cbd7805b1d6ce3ca6a26b2b8c04ad67cc3efe60
David-5-5 <13910767907@139.com>
2024-05-16 15:29:29 +0800

    Add step 1 of step by step

---
+++ src/main/java/dev/morling/onebrc/CalculateAverage_step7.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2023 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package dev.morling.onebrc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static java.lang.Double.parseDouble;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summarizingDouble;
+
+public class CalculateAverage_step7 {
+    public static void main(String[] args) throws Exception {
+        var start = System.currentTimeMillis();
+        calculate();
+        System.err.format("Took %,d ms\n", System.currentTimeMillis() - start);
+    }
+
+    private static void calculate() throws Exception {
+        @SuppressWarnings("resource")
+        var allStats = new BufferedReader(new FileReader("measurements.txt"))
+                .lines()
+                .parallel()
+                .collect(
+                        groupingBy(line -> line.substring(0, line.indexOf(';')),
+                                summarizingDouble(line -> parseDouble(line.substring(line.indexOf(';') + 1)))));
+        var result = allStats.entrySet().stream().collect(Collectors.toMap(
+                Entry::getKey,
+                e -> {
+                    var stats = e.getValue();
+                    return String.format("%.1f/%.1f/%.1f",
+                            stats.getMin(), stats.getAverage(), stats.getMax());
+                },
+                (l, r) -> r,
+                TreeMap::new));
+        System.out.println(result);
+    }
+}

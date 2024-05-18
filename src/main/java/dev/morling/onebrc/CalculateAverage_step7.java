9233e82ee8f474afd99d25a32be6d99af1ddd56b
David-5-5 <13910767907@139.com>
2024-05-18 22:40:01 +0800

    Add inner Step2 class in progress

--- src/main/java/dev/morling/onebrc/CalculateAverage_step7.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_step7.java
@@ -15,27 +15,40 @@
  */
 package dev.morling.onebrc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
-
 import static java.lang.Double.parseDouble;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summarizingDouble;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.channels.FileChannel.MapMode;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 public class CalculateAverage_step7 {
+    private static final String FILE = "./measurements.txt";
+
+    static final File file = new File("measurements.txt");
+    static final long length = file.length();
+    static final int chunkCount = Runtime.getRuntime().availableProcessors();
+    final long[] chunkStartOffsets = new long[chunkCount];
+
+
     public static void main(String[] args) throws Exception {
         var start = System.currentTimeMillis();
         Step1.calculate();
         System.err.format("Took %,d ms\n", System.currentTimeMillis() - start);
     }
 
-    private static class Step1 {
+    static class Step1 {
         private static void calculate() throws Exception {
             @SuppressWarnings("resource")
-            var allStats = new BufferedReader(new FileReader("measurements.txt"))
+            var allStats = new BufferedReader(new FileReader(FILE))
                     .lines()
                     .parallel()
                     .collect(
@@ -53,4 +66,46 @@ public class CalculateAverage_step7 {
             System.out.println(result);
         }
     }
+
+    static class Step2 {
+        
+
+        private static void calculate() throws Exception {
+            final var results = new StationStats[chunkCount][];
+
+            try (var file = new RandomAccessFile(new File(FILE), "r")) {
+                MemorySegment mappedFile = file.getChannel().map(
+                        MapMode.READ_ONLY, 0, length, Arena.global());
+            }
+        }
+    }
+
+
+    static class StationStats implements Comparable<StationStats> {
+        String name;
+        long sum;
+        int count;
+        int min;
+        int max;
+
+        StationStats(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%.1f/%.1f/%.1f", min / 10.0, Math.round((double) sum / count) / 10.0, max / 10.0);
+        }
+
+        @Override
+        public boolean equals(Object that) {
+            return that.getClass() == StationStats.class && ((StationStats) that).name.equals(this.name);
+        }
+
+        @Override
+        public int compareTo(StationStats that) {
+            return name.compareTo(that.name);
+        }
+    }    
+
 }

9ba4d700e8421543d9f62199d2783d771d082847
David-5-5 <13910767907@139.com>
2024-06-10 21:04:58 +0800

    Finish step 5

--- src/main/java/dev/morling/onebrc/CalculateAverage_step7.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_step7.java
@@ -61,7 +61,7 @@ public class CalculateAverage_step7 {
 
     public static void main(String[] args) throws Exception {
         var start = System.currentTimeMillis();
-        Step4.calculate();
+        Step5.calculate();
         System.err.format("Took %,d ms\n", System.currentTimeMillis() - start);
     }
 
@@ -394,20 +394,32 @@ public class CalculateAverage_step7 {
     }
 
     static class StatsAcc {
-        /**
-         * @since Step4
-         */
-        long[] name;
 
         long nameOffset;
         int nameLen;
-        int hash;
+        long hash;
         int sum;
         int count;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+        /**
+         * @since Step4
+         */
+        long[] name;
+        // end of added by Step4
 
-        StatsAcc(int hash, long nameOffset, int nameLen) {
+        /**
+         * @since Step5
+         */
+        private static final long[] emptyTail = new long[0];
+        private static final int NAMETAIL_OFFSET = 2 * Long.BYTES;
+        boolean exportNameString2 = false;
+        long nameWord0;
+        long nameWord1;
+        long[] nameTail;
+        // end of added by Step5
+
+        StatsAcc(long hash, long nameOffset, int nameLen) {
             this.hash = hash;
             this.nameOffset = nameOffset;
             this.nameLen = nameLen;
@@ -424,7 +436,7 @@ public class CalculateAverage_step7 {
          * @since Step4
          */
         public StatsAcc(long inputBase, long hash, long nameStartOffset, int nameLen, long lastNameWord) {
-            this.hash = (int) hash;
+            this.hash = hash;
             this.nameLen = nameLen;
             name = new long[(nameLen - 1) / 8 + 1];
             for (int i = 0; i < name.length - 1; i++) {
@@ -433,11 +445,82 @@ public class CalculateAverage_step7 {
             name[name.length - 1] = lastNameWord;
         }
 
+        /**
+         * 
+         * @param inputBase
+         * @param hash
+         * @param nameStartOffset
+         * @param nameLen
+         * @param nameWord0
+         * @param nameWord1
+         * @param lastNameWord
+         * 
+         * @since Step5
+         */
+        public StatsAcc(long inputBase, long hash, long nameStartOffset, int nameLen,
+                        long nameWord0, long nameWord1, long lastNameWord) {
+            this.hash = hash;
+            this.nameLen = nameLen;
+            this.nameWord0 = nameWord0;
+            this.nameWord1 = nameWord1;
+            int nameTailLen = (nameLen - 1) / 8 - 1;
+            if (nameTailLen > 0) {
+                nameTail = new long[nameTailLen];
+                int i = 0;
+                for (; i < nameTailLen - 1; i++) {
+                    nameTail[i] = getLong(inputBase, nameStartOffset + (i + 2L) * Long.BYTES);
+                }
+                nameTail[i] = lastNameWord;
+            }
+            else {
+                nameTail = emptyTail;
+            }
+            exportNameString2 = true;
+        }
+
         public boolean nameEquals(MemorySegment chunk, long otherNameOffset, long otherNameLimit) {
             var otherNameLen = otherNameLimit - otherNameOffset;
             // Avoid construct instance of String for performance
             return nameLen == otherNameLen &&
                     chunk.asSlice(nameOffset, nameLen).mismatch(chunk.asSlice(otherNameOffset, nameLen)) == -1;
+        }
+
+        /**
+         * 
+         * @param nameWord0
+         * @param nameWord1
+         * @return
+         * @since Step5
+         */
+        boolean nameEquals2(long nameWord0, long nameWord1) {
+            return this.nameWord0 == nameWord0 && this.nameWord1 == nameWord1;
+        }
+
+        /**
+         * 
+         * @param inputBase
+         * @param inputNameStart
+         * @param inputNameLen
+         * @param inputWord0
+         * @param inputWord1
+         * @param lastInputWord
+         * @return
+         * @since Step5
+         */
+        boolean nameEquals(long inputBase, long inputNameStart, long inputNameLen, long inputWord0, long inputWord1, long lastInputWord) {
+            boolean mismatch0 = inputWord0 != nameWord0;
+            boolean mismatch1 = inputWord1 != nameWord1;
+            boolean mismatch = mismatch0 | mismatch1;
+            if (mismatch | inputNameLen <= NAMETAIL_OFFSET) {
+                return !mismatch;
+            }
+            int i = NAMETAIL_OFFSET;
+            for (; i <= inputNameLen - Long.BYTES; i += Long.BYTES) {
+                if (getLong(inputBase, inputNameStart + i) != nameTail[(i - NAMETAIL_OFFSET) / 8]) {
+                    return false;
+                }
+            }
+            return i == inputNameLen || lastInputWord == nameTail[(i - NAMETAIL_OFFSET) / 8];
         }
 
         /**
@@ -488,8 +571,28 @@ public class CalculateAverage_step7 {
          * @since Step4
          */
         String exportNameString() {
+            if (exportNameString2)
+                return exportNameString2();
             var buf = ByteBuffer.allocate(name.length * 8).order(ByteOrder.LITTLE_ENDIAN);
             for (long nameWord : name) {
+                buf.putLong(nameWord);
+            }
+            buf.flip();
+            final var bytes = new byte[nameLen - 1];
+            buf.get(bytes);
+            return new String(bytes, StandardCharsets.UTF_8);
+        }
+
+        /**
+         * 
+         * @return
+         * @since Step5
+         */
+        private String exportNameString2() {
+            var buf = ByteBuffer.allocate((2 + nameTail.length) * 8).order(ByteOrder.LITTLE_ENDIAN);
+            buf.putLong(nameWord0);
+            buf.putLong(nameWord1);
+            for (long nameWord : nameTail) {
                 buf.putLong(nameWord);
             }
             buf.flip();
@@ -620,6 +723,7 @@ public class CalculateAverage_step7 {
      * 幸运的是，我们可以使用反射来获取实例
      */
     private static final Unsafe UNSAFE = unsafe();
+
     private static Unsafe unsafe() {
         try {
             ;
@@ -778,6 +882,235 @@ public class CalculateAverage_step7 {
 
             private static long hash(long prevHash, long word) {
                 return Long.rotateLeft((prevHash ^ word) * 0x51_7c_c1_b7_27_22_0a_95L, 13);
+            }
+
+            private static final long DOT_BITS = 0x10101000;
+
+            // credit: merykitty
+            // The 4th binary digit of the ascii of a digit is 1 while
+            // that of the '.' is 0. This finds the decimal separator.
+            // The value can be 12, 20, 28
+            private static int dotPos(long word) {
+                return Long.numberOfTrailingZeros(~word & DOT_BITS);
+            }
+
+            private static final long MAGIC_MULTIPLIER = (100 * 0x1000000 + 10 * 0x10000 + 1);
+
+            // credit: merykitty and royvanrijn
+            private static int parseTemperature(long numberBytes, int dotPos) {
+                // numberBytes contains the number: X.X, -X.X, XX.X or -XX.X
+                final long invNumberBytes = ~numberBytes;
+
+                // Calculates the sign
+                final long signed = (invNumberBytes << 59) >> 63;
+                final int _28MinusDotPos = (dotPos ^ 0b11100);
+                final long minusFilter = ~(signed & 0xFF);
+                // Use the pre-calculated decimal position to adjust the values
+                final long digits = ((numberBytes & minusFilter) << _28MinusDotPos) & 0x0F000F0F00L;
+
+                // Multiply by a magic (100 * 0x1000000 + 10 * 0x10000 + 1), to get the result
+                final long absValue = ((digits * MAGIC_MULTIPLIER) >>> 32) & 0x3FF;
+                // And apply the sign
+                return (int) ((absValue + signed) ^ signed);
+            }
+        }
+    }
+
+    /**
+     * Optimization 5: Win with statistics
+     * 
+     * upgrade from Step4
+     * 
+     * Duraton :
+     */
+    static class Step5 {
+
+        static void calculate() throws Exception {
+            final var results = new StationStats[chunkCount][];
+            @SuppressWarnings("unchecked")
+            final Future<StationStats[]>[] futures = new Future[chunkCount];
+
+            final var chunkStartOffsets = new long[chunkCount];
+
+            try (var file = new RandomAccessFile(new File(FILE), "r")) {
+                for (int i = 1; i < chunkStartOffsets.length; i++) {
+                    var start = length * i / chunkStartOffsets.length;
+                    file.seek(start);
+                    while (file.read() != (byte) '\n') {
+                    }
+                    start = file.getFilePointer();
+                    chunkStartOffsets[i] = start;
+                }
+                MemorySegment mappedFile = file.getChannel().map(
+                        MapMode.READ_ONLY, 0, length, Arena.global());
+
+                for (int i = 0; i < chunkCount; i++) {
+                    final long chunkStart = chunkStartOffsets[i];
+                    final long chunkLimit = (i + 1 < chunkCount) ? chunkStartOffsets[i + 1] : length;
+                    futures[i] = executorService
+                            .submit(new ChunkProcessor(mappedFile.asSlice(chunkStart, chunkLimit - chunkStart)));
+                }
+
+                for (int i = 0; i < chunkCount; i++) {
+                    results[i] = futures[i].get();
+                }
+                executorService.shutdown();
+
+                var totalsMap = new TreeMap<String, StationStats>();
+                for (var statsArray : results) {
+                    for (var stats : statsArray) {
+                        totalsMap.merge(stats.name, stats, (old, curr) -> {
+                            old.count += curr.count;
+                            old.sum += curr.sum;
+                            old.min = Math.min(old.min, curr.min);
+                            old.max = Math.max(old.max, curr.max);
+                            return old;
+                        });
+                    }
+                }
+                System.out.println(totalsMap);
+
+            }
+        }
+
+        static class ChunkProcessor extends AbstractCallable {
+            private static final int HASHTABLE_SIZE = 4096;
+            private final StatsAcc[] hashtable = new StatsAcc[HASHTABLE_SIZE];
+            private final long inputBase;
+            private final long inputSize;
+
+            ChunkProcessor(MemorySegment chunk) {
+                super(chunk);
+                inputBase = chunk.address();
+                inputSize = chunk.byteSize();
+            }
+
+            @Override
+            public StationStats[] call() throws Exception {
+                long cursor = 0;
+                long lastNameWord;
+                while (cursor < inputSize) {
+                    long nameStartOffset = cursor;
+                    long nameWord0 = getLong(nameStartOffset);
+                    long nameWord1 = getLong(nameStartOffset + Long.BYTES);
+                    long matchBits0 = semicolonMatchBits(nameWord0);
+                    long matchBits1 = semicolonMatchBits(nameWord1);
+
+                    int temperature;
+                    StatsAcc acc;
+                    long hash;
+                    int nameLen;
+                    if ((matchBits0 | matchBits1) != 0) {
+                        int nameLen0 = nameLen(matchBits0);
+                        int nameLen1 = nameLen(matchBits1);
+                        nameWord0 = maskWord(nameWord0, matchBits0);
+                        // bit 3 of nameLen0 is on iff semicolon is not in nameWord0.
+                        // this broadcasts bit 3 across the whole long word.
+                        long nameWord1Mask = (long) nameLen0 << 60 >> 63;
+                        // nameWord1 must be zero if semicolon is in nameWord0
+                        nameWord1 = maskWord(nameWord1, matchBits1) & nameWord1Mask;
+                        nameLen1 &= (int) (nameWord1Mask & 0b111);
+                        nameLen = nameLen0 + nameLen1 + 1; // we'll include the semicolon in the name
+                        lastNameWord = (nameWord0 & ~nameWord1Mask) | nameWord1;
+
+                        cursor += nameLen;
+                        long tempWord = getLong(cursor);
+                        int dotPos = dotPos(tempWord);
+                        temperature = parseTemperature(tempWord, dotPos);
+
+                        cursor += (dotPos >> 3) + 3;
+                        hash = hash(nameWord0);
+                        acc = findAcc2(hash, nameWord0, nameWord1);
+                        if (acc != null) {
+                            acc.observe(temperature);
+                            continue;
+                        }
+                    }
+                    else {
+                        hash = hash(nameWord0);
+                        nameLen = 2 * Long.BYTES;
+                        while (true) {
+                            lastNameWord = getLong(nameStartOffset + nameLen);
+                            long matchBits = semicolonMatchBits(lastNameWord);
+                            if (matchBits != 0) {
+                                nameLen += nameLen(matchBits) + 1;
+                                lastNameWord = maskWord(lastNameWord, matchBits);
+                                cursor += nameLen;
+                                long tempWord = getLong(cursor);
+                                int dotPos = dotPos(tempWord);
+                                temperature = parseTemperature(tempWord, dotPos);
+                                cursor += (dotPos >> 3) + 3;
+                                break;
+                            }
+                            nameLen += Long.BYTES;
+                        }
+                    }
+                    ensureAcc(hash, nameStartOffset, nameLen, nameWord0, nameWord1, lastNameWord).observe(temperature);
+                }
+
+                return Arrays.stream(hashtable)
+                        .filter(Objects::nonNull)
+                        .map(StationStats::new)
+                        .toArray(StationStats[]::new);
+            }
+
+            private StatsAcc findAcc2(long hash, long nameWord0, long nameWord1) {
+                int slotPos = (int) hash & (HASHTABLE_SIZE - 1);
+                var acc = hashtable[slotPos];
+                if (acc != null && acc.hash == hash && acc.nameEquals2(nameWord0, nameWord1)) {
+                    return acc;
+                }
+                return null;
+            }
+
+            private StatsAcc ensureAcc(long hash, long nameStartOffset, int nameLen,
+                                       long nameWord0, long nameWord1, long lastNameWord) {
+                int initialPos = (int) hash & (HASHTABLE_SIZE - 1);
+                int slotPos = initialPos;
+                while (true) {
+                    var acc = hashtable[slotPos];
+                    if (acc == null) {
+                        acc = new StatsAcc(inputBase, hash, nameStartOffset, nameLen, nameWord0, nameWord1,
+                                lastNameWord);
+                        hashtable[slotPos] = acc;
+                        return acc;
+                    }
+                    if (acc.hash == hash && acc.nameEquals(inputBase, nameStartOffset, nameLen, nameWord0, nameWord1,
+                            lastNameWord)) {
+                        return acc;
+                    }
+                    slotPos = (slotPos + 1) & (HASHTABLE_SIZE - 1);
+                    if (slotPos == initialPos) {
+                        throw new RuntimeException(String.format("hash %x, acc.hash %x", hash, acc.hash));
+                    }
+                }
+            }
+
+            private long getLong(long offset) {
+                return UNSAFE.getLong(inputBase + offset);
+            }
+
+            private static final long BROADCAST_SEMICOLON = 0x3B3B3B3B3B3B3B3BL;
+            private static final long BROADCAST_0x01 = 0x0101010101010101L;
+            private static final long BROADCAST_0x80 = 0x8080808080808080L;
+
+            private static long semicolonMatchBits(long word) {
+                long diff = word ^ BROADCAST_SEMICOLON;
+                return (diff - BROADCAST_0x01) & (~diff & BROADCAST_0x80);
+            }
+
+            // credit: artsiomkorzun
+            private static long maskWord(long word, long matchBits) {
+                long mask = matchBits ^ (matchBits - 1);
+                return word & mask;
+            }
+
+            private static int nameLen(long separator) {
+                return (Long.numberOfTrailingZeros(separator) >>> 3);
+            }
+
+            private static long hash(long word) {
+                return Long.rotateLeft(word * 0x51_7c_c1_b7_27_22_0a_95L, 17);
             }
 
             private static final long DOT_BITS = 0x10101000;
